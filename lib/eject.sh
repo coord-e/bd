d84635c6c27be82bd40caedf4c20d32b5850ddfa
coord.e <me@coord-e.com>
2018-09-29 00:30:39 +0900

    Change: Recursively search for used bd commands

--- lib/eject.sh
+++ lib/eject.sh
@@ -1,3 +1,23 @@
+bd_used_cmds=()
+
+function bd::util::find_used() {
+  local bd_cmds=($(compgen -A function | grep bd::cmd::))
+  while read line
+  do
+    for cmd in "${bd_cmds[@]}"; do
+      if $1; then
+        local query=${cmd#bd::cmd::}
+      else
+        local query=$cmd
+      fi
+      if [[ "$line" == *"$query"* ]]; then
+        bd_used_cmds+=("$cmd")
+        declare -f $cmd | sed -e 's/.*()//' | bd::util::find_used false
+      fi
+    done
+  done
+}
+
 function bd::eject() {
   local outfile=$1
 
@@ -15,21 +35,13 @@ function bd::eject() {
 
   bd::cmd::progress "Start ejecting \"$BD_SCRIPT\" into \"$outfile\""
 
-  local bd_cmds=($(compgen -A function | grep bd::cmd::))
-  local used_cmds=()
-  while read line
-  do
-    for cmd in "${bd_cmds[@]}"; do
-      if [[ "$line" == *"${cmd#bd::cmd::}"* ]]; then
-        used_cmds+=("$cmd")
-      fi
-    done
-  done < $BD_SCRIPT
+  bd_used_cmds=()
+  bd::util::find_used true < $BD_SCRIPT
 
   echo "#!/usr/bin/env bash" >> $outfile
   echo "readonly BD_EJECTED=true" >> $outfile
   cat  $BD_ROOT/lib/global_variables.sh >> $outfile
-  for cmd in "${used_cmds[@]}"; do
+  for cmd in "${bd_used_cmds[@]}"; do
     local definition=$(declare -f $cmd)
     echo "${definition//bd::cmd::/}" >> $outfile
   done
