a263b5df81477af8b3d4ec32060369699c192c10
coord.e <me@coord-e.com>
2018-09-28 16:56:49 +0900

    [WIP] Change: Implement eject for a while

--- lib/eject.sh
+++ lib/eject.sh
@@ -1,6 +1,19 @@
 outfile=${ARGS[0]}
-progress "Start ejecting \"$SCRIPT\" into \"$outfile\""
+function eject() {
+  progress "Start ejecting \"$SCRIPT\" into \"$outfile\""
 
+  while read line
+  do
+    if [[ $line == bd_import* ]]; then
+      local cmdline=($line)
+      local importing="$(eval "echo ${cmdline[1]}")"
+      cat "$importing" >> $outfile
+      progress "Importing $importing"
+    else
+      echo "$line" >> $outfile
+    fi
+  done < $BD_BIN
+}
 
-
+eject
 exit
