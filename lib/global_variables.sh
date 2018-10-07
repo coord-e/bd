07b511a22a052389a14ca28b88823fb1ec408116
coord.e <me@coord-e.com>
2018-10-08 00:55:37 +0900

    Fix: Add BD_P_ prefix to "preprocessed" variables
    BD_P_ variables are set before startup, and they're immutable after
    setup

--- lib/global_variables.sh
+++ lib/global_variables.sh
@@ -13,9 +13,10 @@ BD_SCRIPT_NAME=$BD_SCRIPT
 bd_total_progress=0
 bd_current_progress=0
 
-BD_DEFAULT_EJECTED_FUNCTIONS=()
+# BD_P_ variables are set before startup, and they're immutable after setup
+BD_P_DEFAULT_EJECTED_FUNCTIONS=()
 
 # Startup code always have to return 0
-BD_STARTUP_CODE=("[ ! -d $BD_CACHE ] && mkdir -p $BD_CACHE || :")
+BD_P_STARTUP_CODE=("[ ! -d $BD_CACHE ] && mkdir -p $BD_CACHE || :")
 
 BD_LOG_LEVEL=${BD_LOG_LEVEL:-info}
