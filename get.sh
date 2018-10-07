9be57e7a89082ac0c88370fa14f98990fa9cb3f0
coord.e <me@coord-e.com>
2018-10-07 11:53:46 +0900

    Add: Show a message before start downloading installer

--- get.sh
+++ get.sh
@@ -3,6 +3,9 @@
 SCRIPT_URL="https://coord-e.github.io/bd/install.sh"
 
 TMP_EXEC_PATH="$(mktemp)" || exit
+
+echo "Downloading installer"
+
 curl -fsSL ${SCRIPT_URL} > "${TMP_EXEC_PATH}" || exit
 chmod +x "${TMP_EXEC_PATH}"
 "${TMP_EXEC_PATH}" < /dev/tty
